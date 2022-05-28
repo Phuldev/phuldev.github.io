05f19713a780f2dd4681446b1612250416ebf6f2
Phuldev <phuldevmandal18@gmail.com>
2022-05-28 19:08:36 +0545

    change title

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer","Backend Developer"],
+        strings: ["Web Developer","Laravel Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer","Web Designer","Backend Developer"],
+        strings: ["Web Developer","Web Designer","Laravel Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
