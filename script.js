272c1928884592cd6e19e6e324f8560a0270c8af
Phuldev Mandal <phuldevmandal18@gmail.com>
2022-07-19 06:24:43 +0545

    error typedjs

---
+++ script.js
@@ -0,0 +1,7 @@
+//typed
+var typed = new Typed('#data', {
+    strings: ['Web Developer', 'Frontend Developer', 'And', 'Laravel Developer'],
+    BackSpeed: 120,
+    typeSpeed: 120,
+    loop: true
+});
