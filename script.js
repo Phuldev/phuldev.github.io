1b47d54da10f0542241f9e56875649d4c0434e8a
Phuldev Mandal <phuldevmandal18@gmail.com>
2022-07-19 06:27:18 +0545

    error change path typedjs

--- script.js
+++
@@ -1,7 +0,0 @@
-//typed
-var typed = new Typed('#data', {
-    strings: ['Web Developer', 'Frontend Developer', 'And', 'Laravel Developer'],
-    BackSpeed: 120,
-    typeSpeed: 120,
-    loop: true
-});
