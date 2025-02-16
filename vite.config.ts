cb7beb8b55aac3fbd2debdef629a394fe974bebf
Phuldev <phuldevmandal18@gmail.com>
2025-02-17 01:35:22 +0545

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: "/", // Since your repo is phuldev.github.io, use "/"
+  plugins: [react()]
 });
